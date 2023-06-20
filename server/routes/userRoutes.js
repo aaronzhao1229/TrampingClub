3f9a6fda54a4078a36e5266693f749c8e31752d7
samNikau <sdransfield@me.com>
2023-06-20 20:37:17 +1200

    backend done

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,48 +6,48 @@ const db = require('../db/userDb')
 const router = express.router()
 
 router.get('/', checkJwt, async (req, res) => {
-    try {
-      const auth0_id = req.user?.sub;
-  
-      if (!auth0_id) {
-        res.send(null);
-        return;
-      }
-  
-      const user = await db.getUser(auth0_id);
-      res.json(user || null);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send(err.message);
+  try {
+    const auth0_id = req.user?.sub
+
+    if (!auth0_id) {
+      res.send(null)
+      return
     }
-  })
 
-  router.post('/', checkJwt, async (req, res) => {
-    try {
-      const auth0_id = req.user?.sub
-      const { username, icon } = req.body
-      const userDetails = {
-        auth0_id,
-        username,
-        icon,
-      }
-  
-      const usernameTaken = await db.userExists(username)
-      if (usernameTaken) throw new Error('Username Taken')
-  
-      await db.createUser(userDetails)
-  
-      res.sendStatus(201)
-    } catch (err) {
-      console.error(err)
-      if (err.message === 'Username Taken') {
-        res.status(403).send('Username Taken')
-      } else {
-        res.status(500).send(err.message)
-      }
+    const user = await db.getUser(auth0_id)
+    res.json(user || null)
+  } catch (err) {
+    console.error(err)
+    res.status(500).send(err.message)
+  }
+})
+
+router.post('/', checkJwt, async (req, res) => {
+  try {
+    const auth0_id = req.user?.sub
+    const { username, name, email, role } = req.body
+    const userDetails = {
+      auth0_id,
+      name,
+      username,
+      email,
+      role,
     }
-  })
 
+    const usernameTaken = await db.userExists(username)
+    if (usernameTaken) throw new Error('Username Taken')
 
+    await db.createUser(userDetails)
+
+    res.sendStatus(201)
+  } catch (err) {
+    console.error(err)
+    if (err.message === 'Username Taken') {
+      res.status(403).send('Username Taken')
+    } else {
+      res.status(500).send(err.message)
+    }
+  }
+})
 
 module.exports = router
