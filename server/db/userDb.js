61463d0871e5ec98791f2ffa1f7c8f90e760a788
samNikau <sdransfield@me.com>
2023-06-20 20:31:08 +1200

    auth0 backend done

--- server/db/userDb.js
+++ server/db/userDb.js
@@ -1,9 +1,39 @@
 const connection = require('./connection')
 
-function getUser(db = connection) {
-    return db('user').select()
+function userExists(username, db = connection) {
+  return db('users')
+    .where('username', username)
+    .then((usersFound) => !!usersFound.length)
+}
+
+async function getUser(id, db = connection) {
+  try {
+    const user = await db('users')
+      .select('name', 'username', 'email', 'role')
+      .where('auth0_id', id)
+      //not sure if we need id and auth0_id? or how to use both correctly
+      .first()
+    if (!user) {
+      throw new Error('User not found')
+    }
+    return user
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+async function createUser(user, db = connection) {
+  try {
+    const result = await db('users').insert(user)
+    return result
+  } catch (error) {
+    console.error('Error while creating user', error)
+    throw new Error('Failed to create user')
+  }
 }
 
 module.exports = {
-    getUser,
+  getUser,
+  userExists,
+  createUser,
 }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,18 +1,53 @@
 const express = require('express')
 
+const checkJwt = require('../auth0')
 const db = require('../db/userDb')
 
 const router = express.router()
 
-router.get('/', (req, res) => {
-    db.getUser()
-    .then((results) => {
-        res.json({user: results})
-    })
-    .catch((err) => {
-        console.log(err)
-        res.status(500).json({message: 'error getting user'})
-    })
-})
+router.get('/', checkJwt, async (req, res) => {
+    try {
+      const auth0_id = req.user?.sub;
+  
+      if (!auth0_id) {
+        res.send(null);
+        return;
+      }
+  
+      const user = await db.getUser(auth0_id);
+      res.json(user || null);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(err.message);
+    }
+  })
+
+  router.post('/', checkJwt, async (req, res) => {
+    try {
+      const auth0_id = req.user?.sub
+      const { username, icon } = req.body
+      const userDetails = {
+        auth0_id,
+        username,
+        icon,
+      }
+  
+      const usernameTaken = await db.userExists(username)
+      if (usernameTaken) throw new Error('Username Taken')
+  
+      await db.createUser(userDetails)
+  
+      res.sendStatus(201)
+    } catch (err) {
+      console.error(err)
+      if (err.message === 'Username Taken') {
+        res.status(403).send('Username Taken')
+      } else {
+        res.status(500).send(err.message)
+      }
+    }
+  })
+
+
 
 module.exports = router
